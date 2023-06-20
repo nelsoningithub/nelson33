3d43ad062f6e8738530564a2f7523d660217de41
nelsoningithub <136841621+nelsoningithub@users.noreply.github.com>
2023-06-20 22:24:57 +0800

    Update state.cpp

--- src/state/state.cpp
+++ src/state/state.cpp
@@ -238,7 +238,7 @@ int State::evaluate(){
       const int py[] = {-1,-1,1,1};
       //auto self_board_self = this->board.board[player];
       //auto self_board_opponent = this->board.board[1-player];
-      const int piece_scores_self[] = {0, 2, 6, 7, 8, 100, 1000000000};
+      const int piece_scores_self[] = {0, 2, 6, 7, 8, 200, 1000000000};
       const int piece_scores_opponent[] = {0, 2, 6, 7, 8, 100, 1000000000};
     
       for (int i = 0; i < BOARD_H; i++) {
